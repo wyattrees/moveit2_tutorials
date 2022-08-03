ff142fb1212af729482af9aca6fea98a1d9b9d05
Wyatt Rees <wyatt.rees@gmail.com>
2022-08-03 15:42:58 -0600

    Add yoshikawa manipulability cost fn, ruckig smoothing

--- doc/examples/kinematics_cost_function/src/kinematics_cost_function_tutorial.cpp
+++ doc/examples/kinematics_cost_function/src/kinematics_cost_function_tutorial.cpp
@@ -38,6 +38,7 @@
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 #include <moveit/robot_state/cartesian_interpolator.h>
 #include <moveit/trajectory_processing/time_optimal_trajectory_generation.h>
+#include <moveit/trajectory_processing/ruckig_traj_smoothing.h>
 
 #include <moveit_msgs/msg/display_robot_state.hpp>
 #include <moveit_msgs/msg/display_trajectory.hpp>
@@ -46,11 +47,20 @@
 
 #include <tf2_eigen/tf2_eigen.hpp>
 #include <moveit_visual_tools/moveit_visual_tools.h>
+#include <iostream>
 
 // All source files that use ROS logging should define a file-specific
 // static const rclcpp::Logger named LOGGER, located at the top of the file
 // and inside the namespace with the narrowest scope (if there is one)
 static const rclcpp::Logger LOGGER = rclcpp::get_logger("kinematics_cost_fn_demo");
+
+double computeDistanceToLine(Eigen::Vector3d line_start, Eigen::Vector3d line_end, Eigen::Vector3d point)
+{
+  Eigen::Vector3d ab = line_end - line_start;
+  Eigen::Vector3d ac = point - line_start;
+  double area = ab.cross(ac).norm();
+  return area / ab.norm();
+}
 
 int main(int argc, char** argv)
 {
@@ -95,7 +105,7 @@ int main(int argc, char** argv)
 
   Eigen::Isometry3d text_pose = Eigen::Isometry3d::Identity();
   text_pose.translation().z() = 1.0;
-  visual_tools.publishText(text_pose, "KinematicsCostFn_Demo", rvt::WHITE, rvt::XLARGE);
+  visual_tools.publishText(text_pose, "IKCostFn_Demo", rvt::WHITE, rvt::XLARGE);
 
   visual_tools.trigger();
 
@@ -158,9 +168,20 @@ int main(int argc, char** argv)
     double cost = compute_l2_norm(proposed_joint_positions, seed_state);
     return weight * cost;
   };
+  const auto getManipulability = [](Eigen::MatrixXd jacobian) {
+    return sqrt((jacobian * jacobian.transpose()).determinant());
+  };
+  const double sing_weight = 0.00001;
+  const auto sing_avoid_cost = [&sing_weight, &getManipulability](const geometry_msgs::msg::Pose& /*goal_pose*/,
+                                                                  const moveit::core::RobotState& solution_state,
+                                                                  moveit::core::JointModelGroup* jmg,
+                                                                  const std::vector<double>& seed_state) {
+    auto jac = solution_state.getJacobian(jmg);
+    return sing_weight / getManipulability(jac);
+  };
 
   // Now, we request an IK solution twice for the same pose. Once with a cost function, and once without.
-  current_state->setFromIK(joint_model_group, target_pose, 0.0, callback_fn, opts, cost_fn);
+  current_state->setFromIK(joint_model_group, target_pose, 0.0, callback_fn, opts, sing_avoid_cost);
   copy_state->setFromIK(joint_model_group, target_pose, 0.0, callback_fn, opts);
 
   std::vector<double> solution;
@@ -207,45 +228,174 @@ int main(int argc, char** argv)
   // We can also specify cost functions when computing robot trajectories that must follow a cartesian path.
   // First, let's change the target pose for the end of the path, so that if the previous motion plan was executed,
   // we still have somewhere to move.
+  auto start_position = move_group.getCurrentState(10.0)->getFrameTransform("panda_link8").translation();
+  std::vector<std::pair<Eigen::Vector3d, Eigen::Vector3d>> lines;
+  EigenSTL::vector_Isometry3d waypoints;
   target_pose.position.y += 0.4;
   target_pose.position.z -= 0.1;
   target_pose.orientation.w = 0;
   target_pose.orientation.x = -1.0;
-  Eigen::Isometry3d target;
-  tf2::fromMsg(target_pose, target);
+  Eigen::Isometry3d target1;
+  tf2::fromMsg(target_pose, target1);
+  lines.push_back(std::pair(start_position, target1.translation()));
+  waypoints.push_back(target1);
 
-  auto start_state = move_group.getCurrentState(10.0);
-  std::vector<moveit::core::RobotStatePtr> traj;
+  target_pose.position.y -= 0.6;
+  Eigen::Isometry3d target2;
+  tf2::fromMsg(target_pose, target2);
+  lines.push_back(std::pair(target1.translation(), target2.translation()));
+  waypoints.push_back(target2);
+
+  target_pose.position.y += 0.3;
+  target_pose.position.x += 0.15;
+  Eigen::Isometry3d target3;
+  tf2::fromMsg(target_pose, target3);
+  lines.push_back(std::pair(target2.translation(), target3.translation()));
+  waypoints.push_back(target3);
+
+  target_pose.position.y -= 0.3;
+  target_pose.position.z -= 0.2;
+  target_pose.position.x -= 0.15;
+  Eigen::Isometry3d target4;
+  tf2::fromMsg(target_pose, target4);
+  lines.push_back(std::pair(target3.translation(), target4.translation()));
+  waypoints.push_back(target4);
+
+  // target_pose.position.x -= 0.25;
+  // Eigen::Isometry3d target4;
+  // tf2::fromMsg(target_pose, target4);
+  // waypoints.push_back(target4);
   moveit::core::MaxEEFStep max_eef_step(0.01, 0.1);
   // Here, were effectively disabling the jump threshold for joints. This is not recommended on real hardware.
   moveit::core::JumpThreshold jump_thresh(0.0);
+  int num_iters = 25;
+  auto ee_link_model = joint_model_group->getLinkModel("panda_link8");
+  double total_manipulability = 0.0;
+  double total_dist_to_lines = 0.0;
+  double max_dist_to_line = 0.0;
+  size_t pts_considered = 0;
+  std::ofstream datafile;
+  std::stringstream filename;
+  filename << "/home/wyatt/data/costfns/bioik_manipulability_" << (node->now().seconds() / 10000.0) << ".csv";
+  datafile.open(filename.str().c_str());
+  datafile << "manipulability,pos_error\n";
+  for (int iter = 0; iter < num_iters; ++iter)
+  {
+    RCLCPP_WARN(LOGGER, "Executing iteration %d", iter + 1);
+    // compute cartesian path for each waypoint individually
+    for (size_t wi = 0; wi < waypoints.size(); ++wi)
+    {
+      auto start_state = move_group.getCurrentState(10.0);
+      std::vector<moveit::core::RobotStatePtr> traj;
+      const auto wp_frac =
+          moveit::core::CartesianInterpolator::computeCartesianPath(start_state.get(), joint_model_group, traj,
+                                                                    ee_link_model, waypoints[wi], true, max_eef_step,
+                                                                    jump_thresh, callback_fn, opts, sing_avoid_cost);
+      if (wp_frac.value != 1.0)
+      {
+        RCLCPP_ERROR(LOGGER, "Only computed %f percent of cartesian path!", wp_frac.value * 100.0);
+      }
+      // Once we've computed the points in our Cartesian path, we need to add timestamps to each point for execution.
+      robot_trajectory::RobotTrajectory rt(start_state->getRobotModel(), PLANNING_GROUP);
+      for (const moveit::core::RobotStatePtr& traj_state : traj)
+        rt.addSuffixWayPoint(traj_state, 0.0);
+      trajectory_processing::TimeOptimalTrajectoryGeneration time_param;
+      time_param.computeTimeStamps(rt, 1.0);
 
-  // The trajectory, traj, passed to computeCartesianPath will contain several waypoints toward
-  // the goal pose, target. For each of these waypoints, the IK solver is queried with the given cost function.
-  const auto frac = moveit::core::CartesianInterpolator::computeCartesianPath(
-      start_state.get(), joint_model_group, traj, joint_model_group->getLinkModel("panda_link8"), target, true,
-      max_eef_step, jump_thresh, callback_fn, opts, cost_fn);
+      // apply smoothing via Ruckig
+      if (!trajectory_processing::RuckigSmoothing::applySmoothing(rt))
+      {
+        RCLCPP_ERROR(LOGGER, "Could not smooth trajectory with Ruckig!");
+      }
 
-  RCLCPP_INFO(LOGGER, "Computed %f percent of cartesian path.", frac.value * 100.0);
+      // display the trajectory
+      visual_tools.deleteAllMarkers();
+      visual_tools.publishTrajectoryLine(rt, joint_model_group);
+      visual_tools.trigger();
 
-  // Once we've computed the points in our Cartesian path, we need to add timestamps to each point for execution.
-  robot_trajectory::RobotTrajectory rt(start_state->getRobotModel(), PLANNING_GROUP);
-  for (const moveit::core::RobotStatePtr& traj_state : traj)
-    rt.addSuffixWayPoint(traj_state, 0.0);
-  trajectory_processing::TimeOptimalTrajectoryGeneration time_param;
-  time_param.computeTimeStamps(rt, 1.0);
+      // Execute the motion
+      moveit_msgs::msg::RobotTrajectory rt_msg;
+      rt.getRobotTrajectoryMsg(rt_msg);
+      move_group.execute(rt_msg);
+      // calculate the average manipulability for the trajectory
+      // and the deviation from the requested line segment
+      for (size_t i = 0; i < rt.size(); ++i)
+      {
+        // manipulability
+        auto state = rt.getWayPoint(i);
+        auto jac = state.getJacobian(joint_model_group);
+        double manip = getManipulability(jac);
+        total_manipulability += manip;
+        // deviation
+        double dist = computeDistanceToLine(lines[wi].first, lines[wi].second,
+                                            state.getFrameTransform("panda_link8").translation());
+        total_dist_to_lines += dist;
+        if (dist > max_dist_to_line)
+        {
+          max_dist_to_line = dist;
+        }
+        datafile << manip << "," << dist << "\n";
+      }
 
-  moveit_msgs::msg::RobotTrajectory rt_msg;
-  rt.getRobotTrajectoryMsg(rt_msg);
+      pts_considered += rt.size();
 
-  visual_tools.deleteAllMarkers();
-  visual_tools.publishTrajectoryLine(rt, joint_model_group);
-  visual_tools.publishText(text_pose, "Cartesian_Path_Goal", rvt::WHITE, rvt::XLARGE);
-  visual_tools.publishAxisLabeled(target_pose, "cartesian_goal_pose");
-  visual_tools.trigger();
+      rt.clear();
+    }
+    // const auto frac = moveit::core::CartesianInterpolator::computeCartesianPath(
+    //   start_state.get(), joint_model_group, traj, , waypoints,
+    //   true, max_eef_step, jump_thresh, callback_fn, opts, sing_avoid_cost);
 
-  // Once we've computed the timestamps, we can pass the trajectory to move_group to execute it.
-  move_group.execute(rt_msg);
+    // RCLCPP_INFO(LOGGER, "Computed %f percent of cartesian path.", frac.value * 100.0);
+
+    // Once we've computed the points in our Cartesian path, we need to add timestamps to each point for execution.
+    // robot_trajectory::RobotTrajectory rt(start_state->getRobotModel(), PLANNING_GROUP);
+    // for (const moveit::core::RobotStatePtr& traj_state : traj)
+    //   rt.addSuffixWayPoint(traj_state, 0.0);
+    // trajectory_processing::TimeOptimalTrajectoryGeneration time_param;
+    // time_param.computeTimeStamps(rt, 1.0);
+
+    // moveit_msgs::msg::RobotTrajectory rt_msg;
+    // rt.getRobotTrajectoryMsg(rt_msg);
+
+    // visual_tools.deleteAllMarkers();
+    // visual_tools.publishTrajectoryLine(rt, joint_model_group);
+    // visual_tools.publishText(text_pose, "Cartesian_Path_Goal", rvt::WHITE, rvt::XLARGE);
+    // visual_tools.publishAxisLabeled(target_pose, "cartesian_goal_pose");
+    // visual_tools.trigger();
+
+    // Once we've computed the timestamps, we can pass the trajectory to move_group to execute it.
+
+    // save some data about the trajectory
+
+    // reset to start pose
+    move_group.setJointValueTarget(start_joint_positions);
+    move_group.setMaxVelocityScalingFactor(0.05);
+    move_group.setMaxAccelerationScalingFactor(0.05);
+
+    bool success = (move_group.plan(my_plan) == moveit::core::MoveItErrorCode::SUCCESS);
+    RCLCPP_INFO(LOGGER, "Visualizing plan 1 (joint space goal with cost function) %s", success ? "" : "FAILED");
+    if (success)
+    {
+      move_group.execute(my_plan);
+    }
+    else
+    {
+      break;
+    }
+  }
+  datafile.close();
+  double pts_dub = static_cast<double>(pts_considered);
+  std::stringstream strm;
+  strm << "For " << num_iters << " iterations:\nAverage manipulability: " << (total_manipulability / pts_dub) << "\n";
+  strm << "Average distance to intended line: " << (total_dist_to_lines / pts_dub) << "\n";
+  strm << "Max distance to line: " << max_dist_to_line << "\n";
+  RCLCPP_INFO(LOGGER, strm.str().c_str());
+  // RCLCPP_INFO(LOGGER, "For this iteration:");
+  // RCLCPP_INFO(LOGGER, "Average manipulability: %f", total_manipulability / pts_dub);
+  // RCLCPP_INFO(LOGGER, "Average distance to line: %f", total_dist_to_lines / pts_dub);
+  // RCLCPP_INFO(LOGGER, "Max distance to line: %f", max_dist_to_line);
+  // RCLCPP_INFO(LOGGER, "Average trajectory manipulability: %f", total_total / static_cast<double>(num_iters));
+
   visual_tools.prompt("Press 'next' in the RvizVisualToolsGui window to end the demo.");
 
   // END_TUTORIAL
